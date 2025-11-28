9bc2ff071e08bf229cefd39ab8f52a62fb444ce0
sti433 <sti.ri433@gmail.com>
2025-11-28 20:40:16 +0330

    Add files via upload

---
+++ Program(3).cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication136
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, i,s;
+            i = 0;
+            s = 0;
+            a = int.Parse(Console.ReadLine());
+            while (a > 0)
+            {
+                s= a % 10;
+                i++;
+                a= a / 10;
+               
+            }
+            if(i%2==0)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+
+               // Console.WriteLine(i);
+            
+            Console.ReadKey();
+        
+        }
+    }
+}
